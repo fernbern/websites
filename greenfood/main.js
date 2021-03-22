89a3e80f282b0d4624814a5f2175e1ea35d2c103
fernbern <70728008+fernbern@users.noreply.github.com>
2021-03-21 18:24:07 -0700

    Create main.js

---
+++ greenfood/main.js
@@ -0,0 +1,5 @@
+var x = 12;
+const modal = document.querySelector('modal');
+const modalBtn = document.querySelector('modalbtn');
+let twelve = 'time';
+const clocktimer = document.querySelector('');
