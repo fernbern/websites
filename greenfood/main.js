ead5c42f1175e6723d0660ba5e51444a8b084a2b
fernbern <70728008+fernbern@users.noreply.github.com>
2021-03-26 11:32:13 -0700

    Update main.js

--- greenfood/main.js
+++ greenfood/main.js
@@ -3,3 +3,4 @@ const modal = document.querySelector('modal');
 const modalBtn = document.querySelector('modalbtn');
 let twelve = 'time';
 const clocktimer = document.querySelector('');
+let animal = 'pet';
